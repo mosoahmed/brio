d834eb2ea24bc47805c60fc6e026cc13f88acfad
m.ahmed <me@mosahmed.com>
2024-05-10 05:16:04 +0800

    Add comments to NotificationType enum Detailed documentation has been added to the NotificationType enumeration;

--- src/notifications/types/notification.types.ts
+++ src/notifications/types/notification.types.ts
@@ -1,3 +1,10 @@
+/**
+ * `NotificationType` is an enumeration that represents the type of notifications.
+ * It has three types of notifications:
+ * - `LEAVE_BALANCE_REMINDER`: A reminder for leave balance.
+ * - `MONTHLY_PAYSLIP`: A notification for the monthly payslip.
+ * - `HAPPY_BIRTHDAY`: A birthday greeting notification.
+ */
 export enum NotificationType {
   LEAVE_BALANCE_REMINDER = 'leave-balance-reminder',
   MONTHLY_PAYSLIP = 'monthly-payslip',
