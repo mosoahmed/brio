3815cb8997cdb4335e64d168648bf5d0bf403bac
m.ahmed <me@mosahmed.com>
2024-05-10 06:01:51 +0800

    Add Mongoose and Notification Modules
    Introduced Mongoose and Notification Modules to the main application module. The application is now configured to connect to a MongoDB instance at 'mongodb://localhost:27017/notification_db' using Mongoose. Also added the NotificationModule to manage notification-related functionality.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { NotificationModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/notification_db'),
+    NotificationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
